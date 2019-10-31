3a5d617e5b577cd308d361a9ef8b3b1e3b0cbaed
Pravin Patel <patelpra337@gmail.com>
2019-10-30 19:18:43 -0500

    update noclist

--- NOCList.playground/Contents.swift
+++ NOCList.playground/Contents.swift
@@ -29,38 +29,81 @@ This message will self destruct in 5 seconds.
 */
 //: ## Step 1
 //: Create constants for each of the above agents and store all their information in a tuple.
-
+let agent = (coverName: "Ethan Hunt", realName: "Tom Cruise", accessLevel: 8, compromised: false)
+let agent2 = (coverName: "Jim Phelps", realName: "Jon Voight", accessLevel: 9, compromised: true)
+let agent3 = (coverName: "Claire Phelps", realName: "Emmanuelle Beart", accessLevel: 5, compromised: false)
+let agent4 = (coverName: "Eugene Kittridge", realName: "Henry Czerny", accessLevel: 10, compromised: true)
+let agent5 = (coverName: "Franz Krieger", realName: "Jean Reno", accessLevel: 4, compromised: false)
+let agent6 = (coverName: "Luther Stickell", realName: "Ving Rhames", accessLevel: 4, compromised: false)
+let agent7 = (coverName: "Sarah Davies", realName: "Kristin Scott Thomas", accessLevel: 5, compromised: true)
+let agent8 = (coverName: "Max RotGrab", realName: "Vanessa Redgrave", accessLevel: 4, compromised: false)
+let agent9 = (coverName: "Hannah Williams", realName: "Ingeborga Dapkūnaitė", accessLevel: 5, compromised: true)
+let agent10 = (coverName: "Jack Harmon", realName: "Emilio Estevez", accessLevel: 6, compromised: true)
+let agent11 = (coverName: "Frank Barnes", realName: "Dale Dye", accessLevel: 9, compromised: false)
 
 
 //: ## Step 2
 //: Place the above constants inside an array. Declare this array as a constant as well.
-
+let miAgents = [agent, agent2, agent3, agent4, agent5, agent6, agent7, agent8, agent9, agent10, agent11]
 
 
 //: ## Step 3
 //: Create a function that calculates the total number of compromised agents. Inside the function, iterate over the array of agents to determine which ones are compromised. Return the total count.
+func compromisedAgents() -> Int {
+    var agentCount = 0
+    for miAgent in miAgents {
+        if miAgent.compromised == true {
+            agentCount += 1
+        }
+    }
+    
+    return agentCount
+}
 
-
-
+compromisedAgents()
+print(compromisedAgents())
 //: ## Step 4
 //: Call the above function to find the total number of compromised agents and then print a sentence that says "# agents have been compromised!" using string interpolation.
-
-
+compromisedAgents()
+print("\(compromisedAgents()) agents have been compromised")
 
 //: ## Step 5
 //: Create a function called "findCleanAgents" that both prints the cover names of all uncompromised agents, as well as returns an array of agents that are uncompromised.
+func findCleanAgents() -> [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] {
+    var cleanAgents: [(coverName: String, realName: String, accessLevel: Int, compromised: Bool)] = []
+    var agentCount = 0
+    for miAgent in miAgents {
+        if miAgent.compromised == false {
+            agentCount += 1
+            cleanAgents.append(miAgent)
+            print(miAgent.coverName)
+        }
+    }
+    
+    return cleanAgents
+}
 
-
-
+findCleanAgents()
 //: ## Step 6
 //: Call the above function to find the total number of clean agents and print a message that says "# clean agents out of # total agents." Use the total number of agents in the array from step 2 as the second number in the string.
-
+let cleanAgents = findCleanAgents()
+print("\(cleanAgents.count) agents out of \(miAgents.count) total agents")
 
 
 //: ## Step 7
 //: Create a function called "findHighRisk" that prints out the real names and access levels of agents with level 8 or higher. If one of these agents is also currently compromised, add `**WARNING** **COMPROMISED**` to the end of the string that includes their name and access level.
 //: - Example: `Jon Voight, level: 9 **WARNING** **COMPROMISED**`
-
+func findHighRisk() {
+    var count = 8
+    for highRiskAgent in miAgents {
+        if highRiskAgent.accessLevel > 7 {
+            if highRiskAgent.compromised == true {
+                count += 1
+                print(<#T##items: Any...##Any#>)
+            }
+        }
+    }
+}
 
 
 //: ## Step 8
